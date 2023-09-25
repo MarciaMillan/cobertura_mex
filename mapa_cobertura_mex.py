d86366c88de1fbe687f7845befb3b091afb0d50b
Marcia <127223094+MarciaMillan@users.noreply.github.com>
2023-09-25 16:03:30 -0300

    Update mapa_cobertura_mex.py

--- mapa_cobertura_mex.py
+++ mapa_cobertura_mex.py
@@ -447,10 +447,11 @@ if str(comuna) != '' and (start_date > end_date) == False:
 	#st.write(select_polygons)
 	#st.write("DataFrame de conteo de puntos fuera de polígonos:")
 	#st.write(counts)
+	st.write('Misiones por comuna: ')
+	st.write(suma_por_comuna)
+	st.write('Misiones dentro del rango: ')
+	st.write(result)
 	
-	#st.write(suma_por_comuna)
-	#st.write(result)
-	st.write([suma_por_comuna,result])
 	
 
 
