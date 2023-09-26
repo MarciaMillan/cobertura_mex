dcb21e9656199bf6dc0a90b0774f04c662d5d9e5
Marcia <127223094+MarciaMillan@users.noreply.github.com>
2023-09-26 18:41:51 -0300

    Update mapa_cobertura_mex.py

--- mapa_cobertura_mex.py
+++ mapa_cobertura_mex.py
@@ -248,8 +248,8 @@ query_sum =  '''SELECT commune, COUNT(distinct id)
   ORDER BY commune ASC'''.format(start_date,end_date,tuple(comunas))
 
 suma_por_comuna= get_sum_of_points_by_commune(query_sum)
-#suma_por_comuna=suma_por_comuna.sort_values(by='commune')
-
+suma_por_comuna['commune']=suma_por_comuna["commune"].apply(lambda x: f"rango urbano {x}")
+suma_por_comuna.commune.lower()
 
 if str(comuna) != '' and (start_date > end_date) == False:
 	data_query_app = '''SELECT
